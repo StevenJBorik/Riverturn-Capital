d3757cf75c07668cd3ffd5efd18e723bc19fb0a9
Steven Borblob <stevenjborik@protonmail.com>
2022-04-26 22:34:48 -0500

    footer changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -147,15 +147,11 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      {/* <div id='footer'>
+      <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
           </p>
-        </div> */}
+        </div>
       </div>
     </div>
   )
